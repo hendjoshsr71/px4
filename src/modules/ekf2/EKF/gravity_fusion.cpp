e0d15ef6af97db927f6f1f2d450321aec2a46f3b
Daniel M. Sahu <danielmohansahu@gmail.com>
2023-01-10 16:30:01 -0500

    Some cleanup - added third axis despite lack of faith in it.

--- src/modules/ekf2/EKF/gravity_fusion.cpp
+++ src/modules/ekf2/EKF/gravity_fusion.cpp
@@ -47,6 +47,9 @@
 
 void Ekf::fuseGravity()
 {
+	// gravity
+	constexpr float g = 9.80665;
+
 	// get latest estimated orientation
 	const float &q0 = _state.quat_nominal(0);
 	const float &q1 = _state.quat_nominal(1);
@@ -57,19 +60,11 @@ void Ekf::fuseGravity()
 	// const float R_ACC_Z = sq(_params.accelerometer_noise);
 	const float R_ACC_Z = sq(1.0); // shrug
 
-	// gravity
-	const float g = 9.80665;
-
-	// get last accelerometer measurement (body frame) - check if we're moving too much to fuse this measurement
-	const float gravity_scale = _accel_vec_filt.norm() / g;
-	if (gravity_scale < 0.9f || gravity_scale > 1.1f) {
-		PX4_WARN("High acceleration maneuver detected - not fusing.");
-		return;
-	}
-	const Vector3f measurement = _accel_vec_filt / gravity_scale;
+	// use last raw accelerometer measurement (body frame)
+	const Vector3f& measurement = _imu_sample_delayed.delta_vel;
 
 	// calculate innovation -> the difference between measured and estimated angles
-	const Vector3f estimate = _R_to_earth.transpose() * Vector3f(0,0,-g);
+	const Vector3f estimate = _R_to_earth.transpose() * Vector3f(0,0,g);
 	const Vector3f innovation = measurement - estimate;
 	
 	PX4_INFO("measurement (x,y,z): (%f, %f, %f)", (double)measurement(0), (double)measurement(1), (double)measurement(2));
@@ -106,11 +101,11 @@ void Ekf::fuseGravity()
 	const float HK26 = P(2,3)*q2;
 	const float HK27 = HK17 + HK26 + P(0,2)*q1 + P(2,2)*q3;
 	const float HK28 = HK0/(HK12*HK24 + HK15*HK22 + HK16*HK25 + HK20*HK27 + R_ACC_Z);
-	// const float HK29 = 4*g;
-	// const float HK30 = P(1,1)*q1 + P(1,2)*q2;
-	// const float HK31 = 16*HK10;
-	// const float HK32 = P(1,2)*q1 + P(2,2)*q2;
-	// const float HK33 = HK29/(HK30*HK31*q1 + HK31*HK32*q2 + R_ACC_Z);
+	const float HK29 = 4*g;
+	const float HK30 = P(1,1)*q1 + P(1,2)*q2;
+	const float HK31 = 16*HK10;
+	const float HK32 = P(1,2)*q1 + P(2,2)*q2;
+	const float HK33 = HK29/(HK30*HK31*q1 + HK31*HK32*q2 + R_ACC_Z);
 	
 	// Observation Jacobians for Roll Axis
 	SparseVector24f<0,1,2,3> Hfusion;
@@ -119,7 +114,7 @@ void Ekf::fuseGravity()
 	Hfusion.at<2>() = -HK3;
 	Hfusion.at<3>() = HK4;
 
-	// Calculate Kalman gains for Roll Axis
+	// Calculate Kalman gains for X Axis
 	Vector24f Kfusion;
 	Kfusion(0) = -HK21*HK6;
 	Kfusion(1) = -HK19*HK21;
@@ -183,9 +178,46 @@ void Ekf::fuseGravity()
 
 	// perform fusion for Pitch axis
 	const bool pitch_fused = measurementUpdate(Kfusion, Hfusion, innovation(1));
-	
+
+	// Observation Jacobians update for Yaw
+	Hfusion.at<0>() = 0;
+	Hfusion.at<1>() = -HK29*q1;
+	Hfusion.at<2>() = -HK29*q2;
+	Hfusion.at<3>() = 0;
+
+	// Kalman gains update for Yaw Axis
+	Kfusion(0) = -HK33*(HK23 + HK7);
+	Kfusion(1) = -HK30*HK33;
+	Kfusion(2) = -HK32*HK33;
+	Kfusion(3) = -HK33*(HK18 + HK26);
+	Kfusion(4) = -HK33*(P(1,4)*q1 + P(2,4)*q2);
+	Kfusion(5) = -HK33*(P(1,5)*q1 + P(2,5)*q2);
+	Kfusion(6) = -HK33*(P(1,6)*q1 + P(2,6)*q2);
+	Kfusion(7) = -HK33*(P(1,7)*q1 + P(2,7)*q2);
+	Kfusion(8) = -HK33*(P(1,8)*q1 + P(2,8)*q2);
+	Kfusion(9) = -HK33*(P(1,9)*q1 + P(2,9)*q2);
+	Kfusion(10) = -HK33*(P(1,10)*q1 + P(2,10)*q2);
+	Kfusion(11) = -HK33*(P(1,11)*q1 + P(2,11)*q2);
+	Kfusion(12) = -HK33*(P(1,12)*q1 + P(2,12)*q2);
+	Kfusion(13) = -HK33*(P(1,13)*q1 + P(2,13)*q2);
+	Kfusion(14) = -HK33*(P(1,14)*q1 + P(2,14)*q2);
+	Kfusion(15) = -HK33*(P(1,15)*q1 + P(2,15)*q2);
+	Kfusion(16) = -HK33*(P(1,16)*q1 + P(2,16)*q2);
+	Kfusion(17) = -HK33*(P(1,17)*q1 + P(2,17)*q2);
+	Kfusion(18) = -HK33*(P(1,18)*q1 + P(2,18)*q2);
+	Kfusion(19) = -HK33*(P(1,19)*q1 + P(2,19)*q2);
+	Kfusion(20) = -HK33*(P(1,20)*q1 + P(2,20)*q2);
+	Kfusion(21) = -HK33*(P(1,21)*q1 + P(2,21)*q2);
+	Kfusion(22) = -HK33*(P(1,22)*q1 + P(2,22)*q2);
+	Kfusion(23) = -HK33*(P(1,23)*q1 + P(2,23)*q2);
+
+	// perform fusion for Pitch axis
+	const bool yaw_fused = measurementUpdate(Kfusion, Hfusion, innovation(2));
+
 	if (roll_fused)
 		PX4_INFO("Successfully fused ROLL measurement.");
 	if (pitch_fused)
 		PX4_INFO("Successfully fused PITCH measurement.");
+	if (yaw_fused)
+		PX4_INFO("Successfully fused YAW measurement.");
 }
